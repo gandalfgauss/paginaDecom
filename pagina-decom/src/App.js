320ed6860bf270defffc599c8306b59803380e24
gandalfgauss <hallidaysantos@gmail.com>
2023-02-28 10:54:37 -0300

    Criando projeto react para pagina do Decom, alterando barra de titulo, icone e texto do titulo

---
+++ pagina-decom/src/App.js
@@ -0,0 +1,19 @@
+import './App.css'; // Importar Estilos do App
+import React from 'react'; // Importar a biblioteca React
+
+function Cabecalho(){
+  return (
+    <div>
+      <p> S</p>
+    </div>
+  );
+}
+
+function App() {
+
+  return (
+    <Cabecalho/>
+  );
+}
+
+export default App;
