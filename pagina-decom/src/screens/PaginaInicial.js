06e5df90ae12fd520b7b26912e97daf5187cd66f
gandalfgauss <hallidaysantos@gmail.com>
2023-03-03 15:33:05 -0300

    Mudando a ideia do site, iniciando a codificacao do cabecalho, cabecalho1 pronto, metade do menu do cabecalho pronto com animacoes

--- pagina-decom/src/screens/PaginaInicial.js
+++ pagina-decom/src/screens/PaginaInicial.js
@@ -1,116 +1,65 @@
-import './PaginaInicial.css'; // Importar PaginaIncial
+import './PaginaInicial.css'; // Importar estilos da PaginaIncial
 import React from 'react'; // Importar a biblioteca React
 
-//Importar Imagens para cabeçalho de educação
-import logoUFOP from "../images/logoUFOP.png";
-import logoDecom from "../images/logoDecom.png";
-import logoPPGC from "../images/logoPPGC.png";
-import logoICEB from "../images/logoICEB.png";
-import logoAreaRestrita from "../images/logoAreaRestrita.png";
 
-function RenderizaListaDeImagens({imagens})
-{   
-    const [activeIndex, setActiveIndex] = React.useState(null);
-  
-    const handleImageHover = (index) => {
-      setActiveIndex(index);
-    };
-  
-    const handleImageLeave = () => {
-      setActiveIndex(null);
-    };
-  
-    return (
-      <div className="image-list" onMouseLeave={handleImageLeave}>
-        {imagens.map((image, index) => (
-          <a href={image.href}> 
-            <img
-                key={index}
-                src={image.url}
-                alt={image.alt}
-                className={`image-item ${activeIndex === index ? "active" : ""}`}
-                onMouseOver={() => handleImageHover(index)}
-            />
-          </a>
-        ))}
-      </div>
-    );
-    
-}
+//Importando Imagens
+import logoDecomCabecalho from "../images/logodecom@2x.png"
 
-
-function CabecalhoEducacao(){
-
-    const imagens = [{url: logoDecom,
-                      alt: "Logo do DECOM",
-                      href: "http://localhost:3000/"},
-                      {url: logoUFOP,
-                      alt: "Logo da UFOP",
-                      href: "https://www.ufop.br/"},
-                      {url: logoICEB,
-                      alt: "Logo do ICEB",
-                      href: "https://iceb.ufop.br/"},
-                      {url: logoPPGC,
-                      alt: "Logo do PPGC",
-                      href: "http://www3.decom.ufop.br/pos/inicio/"},
-                      //{url: logoAreaRestrita,
-                      //alt: "Logo da Área Restrita"}
-                    ]
-    const [activeIndex, setActiveIndex] = React.useState(false);
-  
-    const handleHover = () => {
-        setActiveIndex(true);
-        console.log("passou");
-    };
-                  
-    const handleLeave = () => {
-        setActiveIndex(false);
-    };
-   
-    return (
-        <div id="cabecalhoEducacao" onMouseOver={handleHover} onMouseLeave={handleLeave}>
-            <p className={activeIndex ? "paragrafoEducacao activep": "paragrafoEducacao"}> Educação </p>
-            <RenderizaListaDeImagens imagens={imagens}/>
-            <div className='linhaInferior'></div>
-        </div>
-      );
-}
-
-function Cabecalho(){
-    return(
-        <div id="cabecalho">
-            <img id="logoDecom" src={logoDecom} alt="Logo do Decom"/>
-            <p id="paragrafoCabecalhoDecom"> Departamento de Ciência da Computação </p>
-            <input type="text" placeholder="Pesquisar" id="myInput"/>
-            <a href="http://www3.decom.ufop.br/decom/login/?next=/decom/login/admin/"> <img id="logoAreaRestrita" src={logoAreaRestrita} alt="Área Restrita"/></a>
-        </div>
-    );
-}
-
-function Menu()
+function MenuCabecalho()
 {
-    return(
-        <div className='menu'>
-            <p> Olá</p>
-            <p> Olá</p>
-            <p> Olá</p>
+    return (
+        <div className='menuCabecalho'>    
+            <div className='menuCabecalhoDecom'>
+                <img
+                className="menuCabecalhoDecomImagem"
+                alt="Logo do DECOM"
+                src={logoDecomCabecalho}
+                />
+                <b className="menuCabecalhoDecomTexto">DECOM</b>    
+            </div>
+
+            <div className="menuCabecalhoOpcoes">
+                <div className="textoCabecalhoMenu textoCabecalhoMenuInicio"><a href='http://localhost:3000/'><p>Início</p></a></div>
+                <div className="textoCabecalhoMenu"><a href='http://www3.decom.ufop.br/decom/o-decom/'><p>DECOM</p></a></div>
+                <div className="textoCabecalhoMenu"><a href='http://www3.decom.ufop.br/decom/noticias/noticias/'><p>Notícias e Eventos</p></a></div>
+                <div className="textoCabecalhoMenu"><a href='http://www3.decom.ufop.br/decom/pessoal/professores/'><p>Pessoal</p></a></div>
+                <div className="textoCabecalhoMenu"><a href='http://www3.decom.ufop.br/decom/ensino/bacharelado/'><p>Ensino</p></a></div>
+                <div className="textoCabecalhoMenu"><a href='http://www3.decom.ufop.br/decom/cocic/'><p>COCIC</p></a></div>
+                <div className="textoCabecalhoMenu"><a href='http://www3.decom.ufop.br/decom/decom.wsgi/pesquisa/labs/'><p>Pesquisa</p></a></div>
+                <div className="textoCabecalhoMenu"><a href='http://www3.decom.ufop.br/decom/extensao/projetos/'><p>Extensão</p></a></div>
+                <div className="textoCabecalhoMenu"><a href='http://www3.decom.ufop.br/decom/contato/'><p>Contato</p></a></div>        
+            </div>
         </div>
     )
 }
 
-function Conteudo(){
+function Cabecalho1()
+{
+    return(
+        <div className='cabecalho1'>
+            <b className='textoDecomCabecalho1'> Departamento de Ciência da Computação</b>
+            <a href='https://www.ufop.br/'> <b className='textoUfopCabecalho1'>👉 UFOP </b></a>
+        </div>
+    )
+}
+
+function Cabecalho(){
   return (
-    <div>
-        <Cabecalho/>
-        <div className="exteno">
-            <div id='conteudo'>
-                <CabecalhoEducacao/>
-            </div>
-            <Menu/>
-        </div>        
+    <div className='cabecalho'>
+        <Cabecalho1/>    
+        <MenuCabecalho/>
     </div>
     
   );
+}
+
+function Conteudo()
+{
+    return (
+        <div className='conteudo'>
+            <Cabecalho/>
+        </div>
+    )
 }
 
 
